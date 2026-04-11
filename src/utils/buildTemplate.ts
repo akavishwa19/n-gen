213795b2aadfa5dd4043c2f359e8da5bbd7e313b
Akash Vishwakarma <akash19102001@gmail.com>
2026-04-11 16:07:55 +0530

    feat: add utils to build and render templates

---
+++ src/utils/buildTemplate.ts
@@ -0,0 +1,15 @@
+import renderTemplate from './renderTemplate';
+import { writeFile, readFile } from './file';
+import { Config } from '../types/config';
+
+async function buildTemplate(
+  inputPath: string,
+  outputPath: string,
+  options: Partial<Config>
+) {
+  const inputData = readFile(inputPath);
+  const outputData = renderTemplate(inputData, options);
+  writeFile(outputPath, outputData);
+}
+
+export default buildTemplate;
